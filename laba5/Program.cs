f1a703b5f337ce65f68f57f891c7c6653cbe1104
Elderly Pig <Elderly Pig@ELDERLYPIG>
2023-12-01 22:53:11 +0300

    Добавьте файлы проекта.

---
+++ laba5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Чтение входных данных из файла
+        string[] lines = File.ReadAllLines("Inlet.in");
+        string[] input = lines[0].Split(' ');
+        int N = int.Parse(input[0]);
+        int B = int.Parse(input[1]);
+        int C = int.Parse(input[2]);
+        int[] A = Array.ConvertAll(lines[1].Split(' '), int.Parse);
+
+        // Подсчет количества элементов, делящихся на B и C
+        int count = 0;
+        for (int i = 0; i < N; i++)
+        {
+            if (A[i] % B == 0 && A[i] % C == 0)
+            {
+                count++;
+            }
+        }
+
+        // Запись результата в файл
+        File.WriteAllText("Outlet.out", count.ToString());
+
+        // Уведомление о записи результата в файл
+        Console.WriteLine("Результат был записан в файл Outlet.out.");
+    }
+}
